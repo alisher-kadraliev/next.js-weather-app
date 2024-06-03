2e5e09a6d47912688903404046d6652bef3e0eb5
Alisher Kadraliev <alisherkadraliev@gmail.com>
2024-06-03 10:56:54 +0300

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.config.js
+
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://next-js-weather-app-seven.vercel.app', // Replace with your site's URL
+    generateRobotsTxt: true, // (optional) Enable to generate robots.txt file
+};
+
+module.exports = config;
